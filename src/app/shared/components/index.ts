5a1e32d9a98493f952a5f932b81debdcacb36990
Deepak <deepak@runloyal.com>
2023-12-07 21:17:55 +0530

    Profile detail added

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,10 +1,13 @@
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 import { ProfileSwiperComponent } from './profile-swiper/profile-swiper.component';
 import { RecommendedProfilesComponent } from './recommended-profiles/recommended-profiles.component';
 
 export const components = [
   ProfileSwiperComponent,
   RecommendedProfilesComponent,
+  ProfileDetailsComponent,
 ];
 
 export * from './profile-swiper/profile-swiper.component';
 export * from './recommended-profiles/recommended-profiles.component';
+export * from './profile-details/profile-details.component';
